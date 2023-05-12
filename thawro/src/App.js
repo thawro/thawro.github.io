cd3b156fae205370fcee88ca9bb54184f3874e18
thawro <tomashawro.kontakt@gmail.com>
2023-05-12 22:45:09 +0200

    Added React app with flowers-classification app on the main page

---
+++ thawro/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        src="https://thawro-flowers-classification.hf.space"
+        frameborder="0"
+        width="1400"
+        height="700"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
